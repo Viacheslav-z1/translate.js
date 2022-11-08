584b27b49363d8c527627490e840189bcfb9d147
slavik <slavik.zaliotnyi@gmail.com>
2022-11-08 22:31:46 +0200

    added spiner

--- app/js/main.js
+++ app/js/main.js
@@ -7,7 +7,9 @@ $(function () {
     btnFrom = document.querySelector('.app__btn-sound__from'),
     btnTo = document.querySelector('.app__btn-sound__to'),
     copyTo = document.querySelector('.app__copy-to'),
-    copyFrom = document.querySelector('.app__copy-from');
+    copyFrom = document.querySelector('.app__copy-from'),
+    paste = document.querySelector('.app__paste'),
+    load = document.querySelector('.spinner');
   selectTag.forEach((tag, id) => {
 
     let option = ``;
@@ -27,6 +29,8 @@ $(function () {
   });
 
 
+
+
   btnFrom.addEventListener('click', () => {
     utterance = new SpeechSynthesisUtterance(input.value);
     utterance.lang = selectTag[0].value;
@@ -39,13 +43,13 @@ $(function () {
     speechSynthesis.speak(utterance);
   });
 
-  
+
   all__select.forEach(item => {
     item.addEventListener('input', getTranslateText)
   });
 
 
-  copyTo.addEventListener('click',()=>{
+  copyTo.addEventListener('click', () => {
     navigator.clipboard.writeText(inputTo.value);
     alert('Скопійовано!')
   });
@@ -56,31 +60,117 @@ $(function () {
   });
 
 
+  navigator.clipboard.readText()
+    .then(text => {
+      if (text) {
+        paste.classList.add('show');
+        paste.addEventListener('click', () => {
+          input.value = text;
+          paste.classList.remove('show');
+          paste.classList.add('hide');
+          getTranslateText()
+        })
+      }
+    })
+
+
+  input.addEventListener('input', getTranslateText);
+
+  input.addEventListener('', () => {
+
+  });
 
 
 
-  input.addEventListener('input', getTranslateText)
 
-  function getTranslateText() {
+
+  
+  // function getTranslateText() {
+  //   let translateFrom = selectTag[0].value,
+  //     translateTo = selectTag[1].value,
+  //     apiUrl = `https://api.mymemory.translated.net/get?q=${input.value}&langpair=${translateFrom}|${translateTo}`;
+  //   fetch(apiUrl).then(response => response.json()).then(data => {
+  //     inputTo.value = ``;
+  //     if (data.responseData.translatedText === "QUERY LENGTH LIMIT EXCEDEED. MAX ALLOWED QUERY : 500 CHARS") {
+  //       alert(`Максимум 500 символів!`);
+  //       input.value = '';
+  //     }
+
+  //     if (data.responseStatus == 429) {
+  //       alert('Кількість запитів на сьогодні вичерпано')
+  //     }
+
+  //     if (data.responseStatus === 200) {
+  //       inputTo.value = data.responseData.translatedText;
+  //     } else {
+  //       inputTo.value = ``;
+  //     }
+  //     console.log(data);
+  //     console.log(data.responseData.translatedText);
+  //   });
+
+  // }
+
+  async function  getTranslateText() {
+
     let translateFrom = selectTag[0].value,
       translateTo = selectTag[1].value,
       apiUrl = `https://api.mymemory.translated.net/get?q=${input.value}&langpair=${translateFrom}|${translateTo}`;
-    fetch(apiUrl).then(response => response.json()).then(data => {
-      inputTo.value = ``;
-      if (data.responseData.translatedText === "QUERY LENGTH LIMIT EXCEDEED. MAX ALLOWED QUERY : 500 CHARS"){
+
+
+    let response = await fetch(apiUrl);
+    load.classList.add('show');
+    const reader = response.body.getReader();
+
+    const contentLength = +response.headers.get('Content-Length');
+
+    // Шаг 3: считываем данные:
+    let receivedLength = 0; // количество байт, полученных на данный момент
+    let chunks = []; // массив полученных двоичных фрагментов (составляющих тело ответа)
+    while (true) {
+      const { done, value } = await reader.read();
+
+      if (done) {
+        break;
+      }
+
+      chunks.push(value);
+      receivedLength += value.length;
+
+
+    }
+
+    // Шаг 4: соединим фрагменты в общий типизированный массив Uint8Array
+    let chunksAll = new Uint8Array(receivedLength); // (4.1)
+    let position = 0;
+    for (let chunk of chunks) {
+      chunksAll.set(chunk, position); // (4.2)
+      position += chunk.length;
+    }
+
+    // Шаг 5: декодируем Uint8Array обратно в строку
+    let result = new TextDecoder("utf-8").decode(chunksAll);
+
+    // Готово!
+    load.classList.remove('show');
+    load.classList.add('hide');
+    let data = JSON.parse(result);
+
+      if (data.responseData.translatedText === "QUERY LENGTH LIMIT EXCEDEED. MAX ALLOWED QUERY : 500 CHARS") {
         alert(`Максимум 500 символів!`);
         input.value = '';
       }
-       
+
+      if (data.responseStatus == 429) {
+        alert('Кількість запитів на сьогодні вичерпано')
+      }
+
       if (data.responseStatus === 200) {
         inputTo.value = data.responseData.translatedText;
       } else {
         inputTo.value = ``;
       }
-      console.log(data);
-      console.log(data.responseData.translatedText);
-    });
+    }
 
-  }
 
 })
